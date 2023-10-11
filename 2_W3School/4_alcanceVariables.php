cdd5c27118a95c0632ea7a6d59a4558dedf1e9c8
BOTHIKIGAI <estebax89@gmail.com>
2023-10-11 15:14:12 -0500

    Actualización - Alcance de las variables - Static 	modified:   4_alcanceVariables.php

--- 2_W3School/4_alcanceVariables.php
+++ 2_W3School/4_alcanceVariables.php
@@ -54,13 +54,64 @@
         }
 
         myThirdTest();
-        echo $b; // la salida sera 15
+        echo $b . "<br>"; // la salida sera 15
 
         /*
         
         Cuarta Prueba - Almacenar en matriz
         
+        Se puede acceder a esta matriz desde dentro de las funciones y se puede utilizar 
+        para actualizar variables globales directamente.
+
+        $GLOBALS es un arreglo asociativo superglobal que almacena todas las variables
+        gloales disponibles en un script PHP. Puedes acceder a las variables globales
+        utilizando la notació de array y el nombre de la variale como clave de arreglo.
+
         */
+
+        $w = 5;
+        $v = 10;
+
+        function myFourthTest(){
+
+            $GLOBALS['v'] = $GLOBALS['w'] + $GLOBALS['v'];
+
+        }
+
+        myFourthTest();
+
+        echo "$v <br>"; // 
+
+        /*
+        
+        Quinta Pruea - Static
+
+        La palabra reservada static se utiliza en el contexto de una función para
+        crear una variables estatica. Una variable estática es una variable que retiene
+        su valor entre llamadas sucesivas a la función. 
+
+        Esto significa que, a diferencia de las variables locales regulares que se
+        reinician cada vez que se llama a la función, una variable estatica conserva su
+        valor incluso despues de que la función haya terminado.
+
+        */
+
+        // Ejemplo
+
+        function myFifthTest() {
+
+            static $t = 0; // Inicializo la variable $t en 0 y siendo static
+            echo $t; // Imprimo la variable $x
+            $t++; // Aumento el valor de la variable $t mas 1
+
+        }
+
+        // Para demostrar el funcionamiento de static ejecutare la funcion myFifthTest tres veces
+        myFifthTest(); // imprime 0
+        myFifthTest(); // imprime 1
+        myFifthTest(); // imprime 2
+
+        // Por cada vez que se ejecuta su valor permanecio y aumento +1
 
     ?>
 
